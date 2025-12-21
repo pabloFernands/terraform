32c3dd084aac2bf2450eeace116bd5103de579ff
PabloF <pablo.fernandes11041992@gmail.com>
2025-12-21 18:44:17 -0300

    Add s3 on the aws

--- main.tf
+++ main.tf
@@ -1,13 +1,19 @@
 
 terraform {
-    required_providers {
-        aws = {
-            source  = "hashicorp/aws"
-            version = "6.27.0"
-        }
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.27.0"
     }
+  }
+  backend "s3" {
+    bucket = "teste-pablo-storage3"
+    key    = "terraform.tfstate"
+    region = "sa-east-1"
+
+  }
 }
 
 provider "aws" {
-            region = "sa-east-1"
+  region = "sa-east-1"
 }
